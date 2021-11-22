8d448783dc29fa1ef31e6a7f5f83e9b59a9f7eb1
unknown <wuall@hotmail.comgit config --global user.name WalaceHenrique>
2021-11-22 13:17:32 -0300

    ViewModel e LiveData

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.unscramble.ui.game
 
 import android.text.Spannable
@@ -25,9 +9,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
-/**
- * ViewModel containing the app data and methods to process the data
- */
 class GameViewModel : ViewModel() {
     private val _score = MutableLiveData(0)
     val score: LiveData<Int>
@@ -53,8 +34,6 @@ class GameViewModel : ViewModel() {
             spannable
         }
     }
-
-    // List of words used in the game
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
@@ -62,9 +41,6 @@ class GameViewModel : ViewModel() {
         getNextWord()
     }
 
-    /*
-     * Updates currentWord and currentScrambledWord with the next word.
-     */
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
@@ -83,9 +59,6 @@ class GameViewModel : ViewModel() {
         }
     }
 
-    /*
-     * Re-initializes the game data to restart the game.
-     */
     fun reinitializeData() {
         _score.value = 0
         _currentWordCount.value = 0
@@ -93,17 +66,10 @@ class GameViewModel : ViewModel() {
         getNextWord()
     }
 
-    /*
-    * Increases the game score if the player’s word is correct.
-    */
     private fun increaseScore() {
         _score.value = _score.value?.plus(SCORE_INCREASE)
     }
 
-    /*
-    * Returns true if the player word is correct.
-    * Increases the score accordingly.
-    */
     fun isUserWordCorrect(playerWord: String): Boolean {
         if (playerWord.equals(currentWord, true)) {
             increaseScore()
@@ -112,9 +78,6 @@ class GameViewModel : ViewModel() {
         return false
     }
 
-    /*
-    * Returns true if the current word count is less than MAX_NO_OF_WORDS
-    */
     fun nextWord(): Boolean {
         return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
